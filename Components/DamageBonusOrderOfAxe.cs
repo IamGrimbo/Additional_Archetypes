fbda329bba19a7d88bb95f6b2e0efb59d158aec5
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2025-04-05 08:05:19 +0200

    Updated Challenge

--- Components/DamageBonusOrderOfAxe.cs
+++ Components/DamageBonusOrderOfAxe.cs
@@ -4,6 +4,9 @@ using Kingmaker.RuleSystem.Rules;
 using Kingmaker.UnitLogic.Mechanics;
 using Kingmaker.UnitLogic;
 using Kingmaker.PubSubSystem;
+using Kingmaker.Blueprints.Classes.Selection;
+using TabletopTweaks.Core.Utilities;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
 
 namespace Additional_Archetypes.Components
 {
@@ -12,12 +15,17 @@ namespace Additional_Archetypes.Components
     public class DamageBonusOrderOfAxe : UnitFactComponentDelegate,
         IInitiatorRulebookHandler<RuleCalculateWeaponStats>
     {
+        static internal BlueprintUnitFactReference CheckedFact = BlueprintTools.GetBlueprint<BlueprintBuff>("4f0218323ad379248b69de8a9501159f").ToReference< BlueprintUnitFactReference>();
+
         public ContextValue BonusValue;
 
         public void OnEventAboutToTrigger(RuleCalculateWeaponStats evt)
         {
-            int bonus = BonusValue.Calculate(this.Fact.MaybeContext);
-            evt.DamageBonusStat += bonus;
+            if (Owner.HasFact(CheckedFact))
+            {
+                int bonus = BonusValue.Calculate(Fact.MaybeContext);
+                evt.DamageBonusStat += bonus;
+            }
         }
 
         public void OnEventDidTrigger(RuleCalculateWeaponStats evt) { }
